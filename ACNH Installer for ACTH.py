bc45347cb7724c1df5724d4c9c6dc69fb52951df
rice-krispy2 <rice.krispy2095@hotmail.com>
2026-03-15 15:10:56 -0700

    Initial version
    Just downloads Eden at the moment. Will add extraction support as well as key installation

---
+++ ACNH Installer for ACTH.py
@@ -0,0 +1,44 @@
+# ACNH Installer
+# Created March 14th, 2026
+# Created by mitzikritzi2191
+
+# We need to achieve the following:
+# 1, Download eden
+# 2, Move the appropriate archivos a el correct destination
+# 3, Provide any extra features requested.
+
+from pathlib import Path
+import os
+import requests
+import urllib.request
+
+home = Path.home()
+
+eden_url = "https://git.eden-emu.dev/eden-emu/eden/releases/download/v0.2.0-rc1/Eden-Windows-v0.2.0-rc1-amd64-msvc-standard.zip"
+
+file_type = {"downloadformat": "zip"}
+
+file_name = f"{home}\Downloads\Eden.zip"
+
+# Print text to the console
+print("Hello! Welcome to the official installer for ACNH.")
+print("This script will download eden for you and install")
+print("the Prod.keys file and Title.keys file for you!")
+consent = input("May I download the latest eden version? (As of 15/03/2026 that is v0.2.0-rc1!): ")
+
+def downloadEden(consent):
+    if consent == "No" or consent == "no":
+        print("That's okay ^^ I will not download Eden.")
+        print("This program will be terminated. Thank you for using it!")
+    elif consent == "Yes" or consent == "yes"  or consent == "Si" or consent == "Sí":
+        print("Okay! Downloading Eden now...")
+        downloaded_file = requests.get(eden_url, params=file_type)
+        sanity_check = downloaded_file.ok
+        if sanity_check == False:
+            print("Download Failed! Please restart the program.")
+        elif sanity_check == True:
+            print("Download Succeeded! Now saving...")
+            with open(file_name, mode="wb") as file:
+                file.write(downloaded_file.content)
+            print(f"Eden has been downloaded and saved to {file_name}! Now preparing for the next steps in installation!")
+downloadEden(consent)
